d43afed1d4f4bd138df432248a0cad23fea9ee01
G-Lazer <82410325+G-Lazer@users.noreply.github.com>
2024-06-03 20:00:38 -0400

    Update BAADS.bat
    Added quotation marks around variables on line 4 to resolve issues in cases where the user chooses filenames with spaces in them.

--- BAADS.bat
+++ BAADS.bat
@@ -1,5 +1,5 @@
 @echo off
 set /p secret=What is the secret file being hidden? : 
 set /p innocent=What is the innocent looking file it'll be hidden in? : 
-type %secret% > %innocent%:%secret%
+type "%secret%" > "%innocent%:%secret%"
 del "%~f0" & exit
